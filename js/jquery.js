0eaf620ad00c6e70d13e088b42c61255dd74a63b
Manoj K <manojapare@gmail.com>
2017-06-23 15:56:49 +0530

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('#btn').click(function(){
     if (confirm('Are you sure you want to rest the list?')) {
-       window.location = 'http://localhost/rand/reset.php';
+       window.location = '../reset.php';
 } else {
               // nothing!
 }
